85640a1478e90667e01998bbce81c31f4e90f9b1
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-30 19:10:51 +0000

    updated deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,7 +12,23 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  const RicToken = await ethers.getContractFactory("Ric");
 
+  const ricToken = await RicToken.deploy(ethers.utils.parseEther("10000000"));
+  const ric = await ricToken.deployed();
+
+  const ArweavePS = await ethers.getContractFactory("ArweavePS");
+  const arweavePS = await ArweavePS.deploy();
+  const arweaveps = await arweavePS.deployed();
+  const DAOStaking = await ethers.getContractFactory("DaoStaking");
+
+  const DaoStaking = await DAOStaking.deploy(
+    ric.address,
+    arweaveps.address,
+    100 // The staki is locked for only 100 blocks for testing purposeszs
+  );
+  const daoStaking = await DaoStaking.deployed();
+  await arweaveps.setStakingLib(daoStaking.address);
   // We get the contract to deploy
   const CatalogDAOLib = await ethers.getContractFactory("CatalogDaoLib");
   const catalogDAOLib = await CatalogDAOLib.deploy();
@@ -20,11 +36,23 @@ async function main() {
   const CatalogDAO = await ethers.getContractFactory("CatalogDao", {
     libraries: { CatalogDaoLib: catalogdaolib.address },
   });
-
-  const catalogDAO = await CatalogDAO.deploy(302400);
+  // The voting period should be 302400 on Harmony network
+  // It's 10 on Hardhat
+  const catalogDAO = await CatalogDAO.deploy(10, daoStaking.address);
   await catalogDAO.deployed();
+  daoStaking.setCatalogDao(catalogDAO.address);
 
+  const TreasuryBoard = await ethers.getContractFactory("TreasuryBoard");
+  const treasuryBoard = await TreasuryBoard.deploy(
+    ric.address,
+    daoStaking.address,
+    10 // Poll period of 10 blocks for testing
+  );
   console.log("Catalogdao deployed to:", catalogDAO.address);
+  console.log("Ric deployed to:", ric.address);
+  console.log("ArweavePs deployed to:", arweaveps.address);
+  console.log("DaoStaking deployed to:", daoStaking.address);
+  console.log("TreasuryBoard deployed to:", treasuryBoard.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
