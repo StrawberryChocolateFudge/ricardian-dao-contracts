9cc25b388bad0e5173b258aac23663f5a3b66194
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-12-30 19:05:10 +0000

    updated with burn

---
+++ contracts/Ric.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Ric is ERC20 {
+    event Burn(uint256 value, address burner);
+
+    constructor(uint256 initialSupply) ERC20("RIC", "RIC") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+        emit Burn(amount, msg.sender);
+    }
+}
